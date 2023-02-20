5e26c13fe9dd089d5cd0f467a13ac52fa75490f8
SyperOlao <olay1234538@gmail.com>
2023-02-20 23:17:42 +0300

    get or create score

--- bot/user/quiz_service.py
+++ bot/user/quiz_service.py
@@ -3,11 +3,13 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, KeyboardBu
 
 from AdminPanel.backend.models.QuizGroup import QuizGroup
 from AdminPanel.backend.models.Quiz import Quiz
+from AdminPanel.backend.models.Score import Score
 from AdminPanel.backend.models.Answer import Answer
 
 from sqlalchemy.ext.asyncio import AsyncSession
 from init_bot import dp, bot, engine
 from sqlalchemy.future import select
+from sqlalchemy import update
 
 from aiogram import types
 
@@ -59,5 +61,19 @@ async def answer(call):
     answer_query = await get_value_from_query(select(Answer).where(Answer.id == id_answer))
     quiz_query = await get_values_from_query(select(Quiz).where(Quiz.id == answer_query.id_Quiz))
     quiz_group_query = await get_value_from_query(select(QuizGroup).where(QuizGroup.id == quiz_query[0].id_QuizGroup))
-    
-    print("title!!!", quiz_group_query.title)
+    session = AsyncSession(engine, expire_on_commit=False)
+    await get_or_create_score(session, Score, id_user=call["from"].id, id_quiz_group=quiz_group_query.id)
+    await session.execute(update(Score).values(score=Score.score + answer_query.correct))
+    await session.commit()
+
+
+async def get_or_create_score(session, model, id_user, id_quiz_group):
+    instance = await session.execute(select(model).filter_by(id_user=id_user, id_quiz_group=id_quiz_group))
+    instance = instance.scalars().first()
+    if instance:
+        return instance
+    else:
+        instance = model(id_user=id_user, id_quiz_group=id_quiz_group, score=0)
+        session.add(instance)
+        session.commit()
+        return instance
